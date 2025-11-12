e702b6feae810e5a007c6833d10e610db9d7374d
B00ZI <iraamaneyahya@gmail.com>
2025-11-12 15:41:00 +0100

    fetching TMDB

--- src/types/MovieDataType.ts
+++ src/types/MovieDataType.ts
@@ -1,8 +1,24 @@
+export interface moviesDataType {
+  id: number;
+  title: string;
+  overview: string;
+  poster: string;
+  backdrop: string;
+  rating: number;
+  releaseDate: string;
+  language: string;
+  genres: number[];
+  popularity: number;
+}
 
-export  interface moviesDataType{
-        id: number ,
-        name : string,
-        rating:number,
-        date : number ,
-        catig:string[]
-    }
+export interface TMDBMovie  {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  backdrop_path: string;
+  vote_average: number;
+  release_date: string;
+  genre_ids: number[];
+};
+
