261a9d51f0688c9746cfc750cc5beb032803309d
B00ZI <iraamaneyahya@gmail.com>
2025-11-30 18:26:53 +0100

    updated the rate contex to save the full obj istad of just id so it workes with temporory serched movies

--- src/components/Ui/Ratting.tsx
+++ src/components/Ui/Ratting.tsx
@@ -1,16 +1,18 @@
 import { useContext} from 'react';
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { RattingContext } from '../../context/rattingContext';
+import type { moviesDataType } from '../../types/MovieDataType';
 
 
 
-
-export default function Ratting({id}:{id:number }) {
+export default function Ratting({movie}:{movie:moviesDataType }) {
 
   
-
+  
   const{userRatting , Ratte} = useContext(RattingContext)!
   
+const movieratting = userRatting.find(m => m.id === movie.id)?.UserRate || 0
+
   const starlist = [1,2,3,4,5]
 
   console.log (userRatting)
@@ -23,13 +25,13 @@ export default function Ratting({id}:{id:number }) {
     <div className="flex gap-1">
 
       {starlist.map(( star) => {
-        if (star <= userRatting[id]) {
-          return <AiFillStar onClick={() => Ratte(id , star)} size={32} key={star} className="cursor-pointer  text-yellow-400" />
-        } else return <AiOutlineStar onClick={() => Ratte( id ,star)} size={32} key={star} className="cursor-pointer text-yellow-400/30" />
+        if (star <= movieratting!) {
+          return <AiFillStar onClick={() => Ratte(movie , star)} size={32} key={star} className="cursor-pointer  text-yellow-400" />
+        } else return <AiOutlineStar onClick={() => Ratte( movie ,star)} size={32} key={star} className="cursor-pointer text-yellow-400/30" />
       })}
 
       <span className="ml-3 flex items-center text-lg font-semibold text-white">
-        {userRatting[id]}/5
+        {movieratting}/5
       </span>
 
     </div>
--- src/context/rattingContext.tsx
+++ src/context/rattingContext.tsx
@@ -1,28 +1,62 @@
-import { createContext , useState } from "react";
+import { createContext, useState } from "react";
 import type { ReactNode } from "react";
+import type { moviesDataType } from "../types/MovieDataType";
+
+
+interface formated {
+    UserRate: number;
+    id: number;
+    title: string;
+    overview: string;
+    poster: string;
+    backdrop: string;
+    rating: number;
+    date: string;
+    genres: number[];
+}
 
 interface RattingContextType {
-    userRatting: Record<number,number>,
-    Ratte:(id: number , ratting : number) => void
-
+    userRatting: formated[],
+    Ratte: (movie: moviesDataType, ratting: number) => void
 }
+
 
 export const RattingContext = createContext<RattingContextType | null>(null)
 
-export default function RattingContextProvider({children}:{children:ReactNode}){
+export default function RattingContextProvider({ children }: { children: ReactNode }) {
 
-    const [userRatting , setUserRatting] = useState<Record<number,number>>({});
+    const [userRatting, setUserRatting] = useState<formated[]>([]);
 
 
-    function Ratte(id:number , ratting:number){
-       setUserRatting(prev=> ({ ...prev , [id] : ratting}))
+    function Ratte(movie: moviesDataType, ratting: number) {
+
+        const formated = { ...movie, UserRate: ratting }
+
+        setUserRatting((prev) => {
+
+            const x = prev.findIndex(item => item.id === formated.id)
+
+            if (x > -1) {
+
+                return prev.map((item, index) =>
+                    index === x ? formated : item
+                )
+                return [...prev, formated]
+
+            } else {
+                return [...prev, formated]
+            }
+
+        })
+
+
     }
 
 
 
     return (
-        <RattingContext.Provider value={{ userRatting , Ratte}}>
-          {children}
+        <RattingContext.Provider value={{ userRatting, Ratte }}>
+            {children}
         </RattingContext.Provider>
     )
 }
