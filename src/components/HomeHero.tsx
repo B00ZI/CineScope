9e7b69d32ec6992009d12e14608053f8635e6181
B00ZI <iraamaneyahya@gmail.com>
2025-12-07 17:34:34 +0100

    perfomens essu with animations fixed

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -112,7 +112,7 @@ const HomeHero = () => {
     <motion.div
     initial={{opacity:0 , y:50}}
     animate={{opacity:1 , y:0}}
-    transition={{duration:0.7}}
+    transition={{duration:0.4}}
      className="w-full">
       <style>{`
         .swiper-pagination-bullet {
